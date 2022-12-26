92b6bf448cdec1d46ae7d2714e6d9ef698d4a7ce
s33ding <robertomdiniz@protonmail.com>
2022-12-26 20:33:36 -0300

    saving

--- PySpark_read_CSV.py
+++ PySpark_read_CSV.py
@@ -1,4 +1,5 @@
 import pyspark
+import sys
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField, StringType, IntegerType 
 from pyspark.sql.types import ArrayType, DoubleType, BooleanType
@@ -6,7 +7,11 @@ from pyspark.sql.functions import col,array_contains
 
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 
+def psr():
+    csv_nm = input("CSV: ")
+    return spark.read.csv(csv_nm, header=True)
 
-csv_nm = input("CSV: ")
-df = spark.read.csv(csv_nm, header=True)
+csv_fl = sys.argv[1]
+
+df = spark.read.csv(csv_fl, header=True)
 df.show(1)
