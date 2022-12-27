8a11c376f388e0feca000fde4fe3a51c93274651
s33ding <robertomdiniz@protonmail.com>
2022-12-27 00:30:15 -0300

    saving

--- addToBashrc.py
+++ addToBashrc.py
@@ -1,5 +1,12 @@
-alias ii="ipython3 -i" #to execute the script interactively
-alias lk="python3 ~/Ipython/chmod.py"
-alias sdf="ipython3 -i ~/Ipython/see_df.py"
-alias eqry="ipython3 -i ~/Ipython/ex_query.py"
-alias qry="ipython3 -i ~/Ipython/input_query.py"
+#                       IPython
+alias ii="ipython3 -i"
+alias lk="python3 ~/IPython/chmod.py"
+alias pdr="ipython3 -i ~/IPython/Pandas_read_CSV.py"
+alias psr="ipython3 -i ~/IPython/PySpark_read_CSV.py"
+alias eqry="ipython3 -i ~/IPython/MySQL_ex_query.py"
+alias qry="ipython3 -i ~/IPython/MySQL_input_query.py"
+alias rsq="ipython3 -i ~/IPython/RedShift_input_query.py"
+alias eJson="ipython3 ~/IPython/Encrypt_JSON.py"
+alias eJ="ipython3 ~/IPython/Encrypt_JSON.py"
+alias dJ="ipython3 ~/IPython/Decrypt_JSON.py"
+alias psq="ipython3 -i ~/IPython/PySpark_MySQL.py"
