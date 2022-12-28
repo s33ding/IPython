f69c636428e121963846a4131f6ef73928f4809e
s33ding <robertomdiniz@protonmail.com>
2022-12-27 22:11:03 -0300

    saving

--- notes.py
+++ notes.py
@@ -6,5 +6,6 @@ path = '~/Documents/notes/'
 text = input('text: ')
 name = input('name: ').replace(' ', '_')
 name = name + "{:_%y%m%d_%H%M%p}".format(moment)
+
 with open(f'{path}{name}.txt', 'w') as f:
-    f.writelines(text)
+    f.write(text)
