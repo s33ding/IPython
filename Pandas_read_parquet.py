3f2c93e94f4d0fad2aa4803d485b27c3dff13020
s33ding <robertomdiniz@protonmail.com>
2023-01-10 21:46:32 -0300

    saving

--- Pandas_read_parquet.py
+++ Pandas_read_parquet.py
@@ -1,13 +1,18 @@
 import sys
 import pandas as pd
 
-def pdprq(fl_nm):
+def pdprq(fl_nm=""):
+
+    try:
+        fl_nm = sys.argv[1] 
+    except:
+        pass
+
     if fl_nm=="":
         fl_nm = input("PARQUET: ")
         return pd.read_parquet(fl_nm)
     else:
         return pd.read_parquet(fl_nm)
 
-fl_nm = sys.argv[1] 
-df = pdprq(fl_nm)
+df = pdprq()
 print(df)
