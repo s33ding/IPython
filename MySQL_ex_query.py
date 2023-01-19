72c0edc59f51367850c9d46a851aad849bdf7ad5
s33ding <robertomdiniz@protonmail.com>
2023-01-19 20:46:03 -0300

    saving

--- MySQL_ex_query.py
+++ MySQL_ex_query.py
@@ -6,16 +6,16 @@ import mysql.connector
 import os
 
 with open(os.environ["MYSQL_CRED"], 'r') as f:
-    db_mysql = json.load(f)
+    db_ms = json.load(f)
 
-engine_mysql = mysql.connector.connect(
-        host=db_mysql['host'], 
-        user=db_mysql["user"], 
-        password=db_mysql["password"])
+engine_ms = mysql.connector.connect(
+        host=db_ms['host'], 
+        user=db_ms["user"], 
+        password=db_ms["password"])
 
 fl = sys.argv[1] 
 with open(fl, "r") as f:
     qry = f.read()
 
-df = pd.read_sql(qry, engine_mysql)
+df = pd.read_sql(qry, engine_ms)
 print(df)
