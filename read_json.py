dc52340ca6039d575f056bdf0c809341bead08aa
s33ding <robertomdiniz@protonmail.com>
2023-03-03 00:05:34 -0300

    saving

--- read_json.py
+++ read_json.py
@@ -8,14 +8,14 @@ def sv(nm="df"):
     print(f"SAVING:{nm}.parquet")
     df.to_parquet(f"{nm}.parquet")
 
-def readjson(fl_nm):
-    if fl_nm=="":
-        fl_nm = input("JSON: ")
+def rjson(filename=""):
+    if filename=="":
+        filename = input("JSON: ")
     with open(filename, 'r') as f:
         return json.load(f)
 
 filename = sys.argv[1]
 
-data = readjson(filename)
+data = rjson(filename)
 print("\nThe JSON is loaded as a dictionary called 'data'.")
 sleep(2)
