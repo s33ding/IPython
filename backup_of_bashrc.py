7ac5c6b33da5d0885d36a75c0fa34698b58be736
s33ding <robertomdiniz@protonmail.com>
2023-03-07 11:04:18 -0300

    saving

--- backup_of_bashrc.py
+++ backup_of_bashrc.py
@@ -4,6 +4,3 @@ path_file = '~/.bashrc'
 path_bck = os.environ['BASHRC_BCK']
 
 os.system(f'cp {path_file} {path_bck}')
-os.system(f'cd $ADM/config/')
-os.system(f"git add .;git commit -m 'saving'; git push")
-
